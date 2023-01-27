8e235ce30456c71ea12a8303cc9a3b2bcefedcbd
Mehmet <110852769+MiyazakiMehmet@users.noreply.github.com>
2023-01-27 12:44:35 +0300

    Discord Bot

---
+++ PythonApplication1/PythonApplication1/Cogs/classEmbed.py
@@ -0,0 +1,60 @@
+﻿import discord
+from discord.ext import commands
+from discord.utils import get
+
+bot = discord.ext.commands.Bot(command_prefix = "+", intents=discord.Intents.all());
+
+class MyCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+
+    #Displaying Profile Picture
+    @commands.command()
+    async def korembed(self, ctx, *, avamember: discord.Member = None):
+        if avamember == None:
+            embed = discord.Embed(description='❌ Error! Please specify a user',
+                                  color=discord.Color.red())
+            await ctx.reply(embed=embed, mention_author=False)
+        else:
+            userAvatarUrl = avamember.avatar
+            if(ctx.message.mentions[0].id == 206003447655432192): #Checks id of the person who tagged
+                embed = discord.Embed(title="GORAY GURT's Avatar", colour=discord.Colour.red())
+            else:
+                embed = discord.Embed(title=('{}\'s Avatar'.format(avamember.name)), colour=discord.Colour.red())   
+            embed.set_image(url='{}'.format(userAvatarUrl))
+            await ctx.reply(embed=embed, mention_author=False) 
+
+    @bot.command(pass_context=True)
+    async def korkick(self, ctx, user: discord.Member):
+        #if(ctx.message.mentions[0].id == 206003447655432192): #Checks id of the person who tagged
+        await ctx.guild.kick(user)
+
+    #Poll
+    @commands.has_permissions(manage_messages=True)
+    @commands.command(pass_context=True)
+    async def korpoll(self, ctx, question, *options: str):
+
+        if len(options) > 2:
+            await ctx.send('```Error! Syntax = [~poll "question" "option1" "option2"] ```')
+            return
+
+        if len(options) == 2 and options[0] == "yes" and options[1] == "no":
+            reactions = ['👍', '👎']
+        else:
+            reactions = ['👍', '👎']
+
+        description = []
+        for x, option in enumerate(options):
+            description += '\n\n {} {}'.format(reactions[x], option)
+
+        poll_embed = discord.Embed(title=question, color=0x31FF00, description=''.join(description))
+
+        react_message = await ctx.send(embed=poll_embed)
+
+        for reaction in reactions[:len(options)]:
+            await react_message.add_reaction(reaction)
+
+
+async def setup(client):
+    await client.add_cog(MyCog(client))
---
+++ PythonApplication1/PythonApplication1/main.py
@@ -0,0 +1,61 @@
+﻿import discord
+from discord.ext import commands
+from discord.utils import get
+import random
+import os
+import asyncio
+
+client = commands.Bot(command_prefix="+", intents=discord.Intents.all())
+discord_status = "+korhelp"
+
+@client.event
+async def on_ready():
+    print("Bot is connected to discord")
+    await  client.change_presence(activity=discord.Game(discord_status)) 
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@client.command()
+async def korhelp(ctx):
+    read_help = "Korcm Bot is a useful discord bot which provides korcm commands.\n\n -> *+korhelp* - Shows all the commands.\n-> *+korandom* - Generates random koray statement.\n-> *+korpick (number)* - Generates a koray statement that you've picked.\n-> *+korembed* - Displays your profile.\n-> *+korpoll* - Poll"
+   
+    await ctx.send(read_help)
+
+@client.command()
+async def korandom(ctx):
+    with open("d:\\w10\\Desktop\\message.txt", "r") as f:
+        random_response = f.readlines()
+        answer = random.choice(random_response) 
+        answer = answer.replace(' ', '-) ', 1)
+    await ctx.send(answer)
+
+@client.command()
+async def korpick(ctx, number):
+    with open("d:\\w10\\Desktop\\message.txt", "r") as f:
+        line = f.readlines()[int(number) - 1]
+        line = line.replace(' ', '-) ', 1)
+    await ctx.send(line)
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.channel_id == 1068190750115909722:
+         if payload.emoji.name == "👍":
+              channel = client.get_channel(payload.channel_id)
+              message = await channel.fetch_message(payload.message_id)
+              reaction = get(message.reactions, emoji=payload.emoji.name)
+              if reaction and reaction.count >= 5:
+                  await message.delete()
+
+async def load():
+    for filename in os.listdir("./Cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"Cogs.{filename[:-3]}")
+
+async def main():
+    async with client:
+        await load()
+        await client.start("MTA2ODExOTIwOTIyNzMyOTYwNw.GrZ4IT.vL066vqj_9MOARyNAtBtgkZpNWtsdpURRxBf5Y")
+
+asyncio.run(main())
